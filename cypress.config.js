d881705d39c937bcffbe83585d3d562d92e1df3d
Tinita <tino4ka.87@mail.ru>
2024-06-10 19:12:25 +0300

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://qamid.tmweb.ru",
+    retries: 0,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  viewportWidth: 1000,
+  viewportHeight: 660,
+  pageLoadTimeout: 80000,
+});
